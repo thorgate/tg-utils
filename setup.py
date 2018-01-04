b1cc93f375fc94bf8e603f2456ae4e212d6ae064
Rivo Laks <code@rivolaks.com>
2018-01-04 21:48:32 +0200

    Update Python versions in setup.py

--- setup.py
+++ setup.py
@@ -43,10 +43,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
