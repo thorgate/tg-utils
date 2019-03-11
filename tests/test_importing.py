30a453f6ca20f4e667e5f3f1ca47cac3b578d073
Sergey Kovalskiy <sergey@thorgate.eu>
2019-03-11 11:50:56 +0200

    Copy lock.py used in various projects and make it reusable

--- tests/test_importing.py
+++ tests/test_importing.py
@@ -12,6 +12,7 @@ def test_imports():
     from tg_utils import email
     from tg_utils import files
     from tg_utils import hashmodels
+    from tg_utils import lock
     from tg_utils import managers
     from tg_utils import mixins
     from tg_utils import models
