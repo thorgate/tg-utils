4249b0421be8cc7328d8c3ed185ff753deda4e07
Sergey Kovalskiy <sergey@thorgate.eu>
2024-01-08 14:21:56 +0200

    fix: deprecated in django 5.0
    Avoid using deprecated undocumented
    django.utils.datetime_safe, and use
    django.utils.timezone instead

--- tg_utils/health_check/checks/celery_beat/backends.py
+++ tg_utils/health_check/checks/celery_beat/backends.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.core.cache import cache
-from django.utils.datetime_safe import datetime
+from django.utils import timezone
 
 from health_check.backends import BaseHealthCheckBackend
 from health_check.exceptions import ServiceUnavailable
@@ -19,7 +19,23 @@ class CeleryBeatHealthCheck(BaseHealthCheckBackend, HealthCheckSettingsMixin):
         last_beat_time = cache.get(CACHE_KEY, None)
         if last_beat_time is None:
             self.add_error(ServiceUnavailable("Celery beat is not scheduling tasks"))
-        elif (datetime.now() - last_beat_time).seconds > DELAY_THRESHOLD:
+            return
+
+        if (
+            not isinstance(last_beat_time, timezone.datetime)
+            or not last_beat_time.tzinfo
+        ):
+            self.add_error(
+                # If you encounter this error in tests, most likely you are using a fixture
+                # instead of running the task, and the fixture is providing unaware timestamp. Switch
+                # your ficture to locale-aware timestamp to fix this
+                ServiceUnavailable(
+                    f"Celery beat task timestamp is invalid: {last_beat_time!r}"
+                )
+            )
+            return
+
+        if (timezone.now() - last_beat_time).seconds > DELAY_THRESHOLD:
             self.add_error(
                 ServiceUnavailable(
                     f"Celery beat tasks are delayed, last ran at {last_beat_time}."
--- tg_utils/health_check/checks/celery_beat/tasks.py
+++ tg_utils/health_check/checks/celery_beat/tasks.py
@@ -1,5 +1,5 @@
 from django.core.cache import cache
-from django.utils.datetime_safe import datetime
+from django.utils import timezone
 
 from celery import shared_task  # pylint: disable=import-error
 from tg_utils.health_check.checks.celery_beat.backends import CACHE_KEY
@@ -8,4 +8,4 @@ from tg_utils.health_check.checks.celery_beat.backends import CACHE_KEY
 @shared_task(ignore_result=False)
 def timestamp_task():
     # timeout=None causes value to be stored forever
-    cache.set(CACHE_KEY, datetime.now(), timeout=None)
+    cache.set(CACHE_KEY, timezone.now(), timeout=None)
